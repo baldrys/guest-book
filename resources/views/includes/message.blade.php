48aeefd725dcf5c3437737cb2414c3d6660b6b71
baldrys <baldrys@rambler.ru>
2019-08-01 07:38:56 +0300

    изменен формат даты в шаблоне

--- resources/views/includes/message.blade.php
+++ resources/views/includes/message.blade.php
@@ -3,5 +3,5 @@
   <td>{{ $message->email }}</td>
   <td>{{ $message->text }}</td>
   <td>{{ $message->tagsToString() }}</td>
-  <td>{{ $message->created_at }}</td>
+  <td>{{ $message->created_at->format('d.m.Y H:i') }}</td>
 </tr>
